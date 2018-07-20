67a22fe4c15478d77e2269db38696bd3c74f960b
Gor32 <kirakosyan0001@gmail.com>
2018-07-20 19:48:25 +0400

    add Solving problem
    solve problem with DecisionTree and RandomForest algorithms

---
+++ start.py
@@ -0,0 +1,93 @@
+from sklearn import tree
+from sklearn import ensemble
+
+# to use DecisionTreeClassifier or RandomForestClassifier
+useDecisionTreeClassifier = False
+
+# Open the file and read in data from that.
+training_data_file = open("trainingdata.txt")
+theList = []
+for entry in training_data_file:
+    entry = entry.strip()
+    theList.append(entry)
+training_data_file.close()
+
+# Initialize a dictionary
+db = dict()
+
+# Iterate through each line in theList
+i = 0
+count = 0
+
+# Each champion is assigned a unique id
+while i < len(theList):
+    if '1' in theList[i]:  # If team 1 wins
+        champions = theList[i].split(',')
+        champions.pop()
+        j = 0
+        while j < 5:
+            if champions[j] not in db:
+                db[champions[j]] = count
+                count += 1
+            j += 1
+
+    else:  # If team 2 wins
+        champions = theList[i].split(',')
+        champions.pop()
+        j = 5
+        while j < 10:
+            if champions[j] not in db:
+                db[champions[j]] = count
+                count += 1
+            j += 1
+    i += 1
+
+i = 0
+dataChampions = []
+dataWinners = []
+
+while i < len(theList):
+    data = []
+    champions = theList[i].split(',')
+    winner = champions.pop()
+    dataWinners.append(winner)
+    j = 0
+    while j < len(champions):
+        data.append(db[champions[j]])
+        j += 1
+    dataChampions.append(data)
+    i += 1
+
+# Import the classifier
+my_classifier = tree.DecisionTreeClassifier() if useDecisionTreeClassifier  else ensemble.RandomForestClassifier()
+
+# Train the classifier
+my_classifier.fit(dataChampions, dataWinners)
+
+# Test the algorithm
+inp = int(input())
+k = 0
+inputList = []
+while k < inp:
+    line = input()
+    inputList.append(line)
+    k += 1
+
+j = 0
+testChampions = []
+
+while j < len(inputList):
+    stringChampions = inputList[j].split(',')
+    intChampions = []
+    z = 0
+    while z < 10:
+        intChampions.append(db[stringChampions[z]])
+        z += 1
+    testChampions.append(intChampions)
+    j += 1
+
+predictions = my_classifier.predict(testChampions)
+
+# Print the results
+for prediction in predictions:
+    print(prediction)
